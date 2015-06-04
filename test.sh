d3eb8c34e43268838250e33af00a45f3b5839fb8
zhangjialin <cainiaocome@gmail.com>
2015-06-04 09:33:15 +0800

    save update

--- test.sh
+++ test.sh
@@ -1,8 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-current_runlevel=`runlevel | cut -d ' ' -f 2`
-current_rc_d="/etc/rc$current_runlevel.d/"
-inject_file=`ls $current_rc_d | grep 'cron' | tail -n 1`
-inject_file_real_path=$current_rc_d$inject_file
-
-cat $inject_file_real_path | grep '^start)'
+self=`basename $0`
+echo $self
